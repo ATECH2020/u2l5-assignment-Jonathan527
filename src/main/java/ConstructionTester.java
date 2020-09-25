a5ba2eeafe33e8fe8acecc0da920a5db33152c12
Jonathan Camacho <jonathan.2033158@nv.ccsd.net>
2020-09-25 21:04:49 +0000

    Completed ConstructionTester

--- src/main/java/ConstructionTester.java
+++ src/main/java/ConstructionTester.java
@@ -4,6 +4,27 @@ public class ConstructionTester
 {
     public static void main(String[] args)
     {
-        // Start here!
+      int numBoard, numWindows;
+      double salesTaxRate, total, totalCost;
+
+      Scanner in = new Scanner(System.in);
+
+      System.out.println("Enter the sales tax rate: ");
+      salesTaxRate = in.nextDouble();
+
+      System.out.println("How many boards do you need?");
+      numBoard = in.nextInt();
+
+      System.out.println("How many windows do you need?");
+      numWindows = in.nextInt();
+
+      Construction myConstruction = new Construction(8.0, 11.0, salesTaxRate);
+
+      total = myConstruction.lumberCost(numBoard) + myConstruction.windowCost(numWindows);
+
+      totalCost = myConstruction.grandTotal(total);
+
+      System.out.println("Total: " + total);
+      System.out.println("Grand Total: " + totalCost);
     }
 }
